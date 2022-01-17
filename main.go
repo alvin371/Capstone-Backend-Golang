d960191beb50d5b29d45c235af066959020cbd5e
alvinsatria31 <alvin.satria@konsultan.belajar.id>
2022-01-17 12:00:09 +0700

    configuration main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"capstone/backend/driver"
+	_middleware "capstone/backend/middlewares"
+	"capstone/backend/routes"
+)
+
 func main() {
+	driver.InitDB()
+	e := routes.New()
+
+	// Log Middleware
+	_middleware.LogMiddlewareInit(e)
+
+	e.Start(":8000")
 
 }
