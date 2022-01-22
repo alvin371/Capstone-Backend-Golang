7206915fd43208dffb07c9d5a1bc2bc8ad52abfd
barcodelou <yonathanverrel@student.telkomuniversity.ac.id>
2022-01-22 20:06:52 +0700

    send

--- driver/database.go
+++ driver/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	db, err := gorm.Open(mysql.Open("root:@/gym?parseTime=true"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open("root:cobaaaa?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
